f693fdb58193a0e0369205c929645361cb9d3167
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-02-07 16:20:33 -0800

    Update generateMarkdown.js
    removed unnecessary comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-
 function renderLicenseBadge(license) {
   if (!license){
     return "";
@@ -9,8 +6,6 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if(!license){
     return "";
@@ -19,8 +14,6 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(!license){
     return "";
